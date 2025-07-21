3e9dbdf082197f944f629ab23c19a786aced2b56
anup7349 <anupsnaik@gmail.com>
2025-07-21 14:34:42 +0530

    Vehicle details

---
+++ Vehiclee.java
@@ -0,0 +1,27 @@
+class Vehiclee {
+	String model;
+	String shade;
+
+	public Vehicle(String model, String shade) {
+		this.model = model;
+		this.shade = shade;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setShade(String shade) {
+		this.shade = shade;
+	}
+
+	public String getShade() {
+		return shade;
+	}
+
+
+}
