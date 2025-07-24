4ed3f7e89362fe1079215d0d8fd9d20756335965
anup7349 <anupsnaik@gmail.com>
2025-07-24 11:31:46 +0530

    loops

--- ParaConstructor.java
+++
@@ -1,8 +0,0 @@
-class ForLoop{
-	public static void main (String[] args){
-		public final String EXECUTION="Executing for loop";
-		for (int i=0;i<1000;i++){
-			int z=i+1;
-			System.out.println(EXECUTION+z);
-		}
-	}
